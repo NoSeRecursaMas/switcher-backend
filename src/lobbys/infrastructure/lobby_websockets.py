86ed1aa5255fca1f6d8a640448b3146eec8553bc
0s1r4s <benjalozano13@outlook.com>
2024-09-20 18:18:31 -0300

    SW-8 Modulo websocket general

--- src/lobbys/infrastructure/lobby_websockets.py
+++ src/lobbys/infrastructure/lobby_websockets.py
@@ -2,18 +2,24 @@ from fastapi import WebSocket
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: list[WebSocket] = []
+        self.active_room_connections: dict[int, list[WebSocket]] = {}
 
-    async def connect(self, websocket: WebSocket):
+    async def connect_to_room(self, room_id: int, websocket: WebSocket):
         await websocket.accept()
-        self.active_connections.append(websocket)
+        if room_id not in self.active_room_connections:
+            self.active_room_connections[room_id] = []
+        self.active_room_connections[room_id].append(websocket)
 
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
+    def disconnect_from_room(self, room_id: int, websocket: WebSocket):
+        if room_id in self.active_room_connections:
+            self.active_room_connections[room_id].remove(websocket)
+            if not self.active_room_connections[room_id]:
+                del self.active_room_connections[room_id]
 
     async def send_personal_message(self, message: str, websocket: WebSocket):
         await websocket.send_text(message)
-        
-    async def broadcast(self, message: str):
-        for connection in self.active_connections:
-            await connection.send_text(message)
+
+    async def broadcast_to_room(self, room_id: int, message: str):
+        if room_id in self.active_room_connections:
+            for connection in self.active_room_connections[room_id]:
+                await connection.send_text(message)
