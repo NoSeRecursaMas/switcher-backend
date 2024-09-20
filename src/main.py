e369380b714bffb12ebf1ea312e51073438639e7
0s1r4s <benjalozano13@outlook.com>
2024-09-20 18:10:37 -0300

    SW-8 CORS agregada, correccion de tipeo

--- src/main.py
+++ src/main.py
@@ -1,14 +1,30 @@
-from fastapi import FastAPI
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse
 from src.players.infrastructure.api import router as players_router
 from src.database import engine, Base
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(
+    title="Switcher Card Game",
+    description="API for Switcher Card Game"
+)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/", tags=["Root"])
+def redirect_to_docs():
+    return RedirectResponse(url="/docs/")
 
 app.include_router(players_router, prefix="/players", tags=["players"])
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0",reload=True, port=8000)
