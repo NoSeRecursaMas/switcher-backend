5c38afebe3556096b27abd8d174f52f0d4ebf6c6
Lozano-Benjamin <benjalozano13@outlook.com>
2024-09-22 14:45:33 -0300

    SW-8 Websocket endpoint pero sin completar

--- src/lobbys/infrastructure/api.py
+++ src/lobbys/infrastructure/api.py
@@ -1,12 +1,26 @@
+from fastapi import APIRouter, Depends, WebSocket, WebSocketDisconnect
 from src.database import get_db
-from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from src.lobbys.application.service import LobbyService
 from src.players.infrastructure.repository import SQLAlchemyRepository as PlayerSQLAlchemyRepository
 from src.lobbys.infrastructure.repository import SQLAlchemyRepository as LobbySQLAlchemyRepository
 from src.lobbys.domain.models import LobbyResponse, CreteLobbyRequest
+from src.lobbys.infrastructure.lobby_websockets import ConnectionManager
 
 router = APIRouter()
+
+manager = ConnectionManager
+
+@router.websocket("/ws/{game_id}/{player_id}")
+async def websocket_endpoint(websocket: WebSocket, game_id: int, player_id: int):
+    await manager.connect_to_room(room_id=game_id,player_id=player_id,websocket=websocket)
+    try:
+        while True:
+            data = await websocket.receive_json()
+            if data["type"] == "update":
+                pass
+    except WebSocketDisconnect:
+        manager.disconnect_from_room(room_id=game_id,player_id=player_id,websocket=websocket)
 
 
 @router.post("", status_code=201)
