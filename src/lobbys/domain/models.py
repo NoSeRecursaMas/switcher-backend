ea1e20620cf2af7ee925fbecee78e00326239a25
Lozano-Benjamin <benjalozano13@outlook.com>
2024-09-22 15:04:13 -0300

    SW-8 Creado RoomInfo para actualizar la informacion de los jugadores que entran a la sala

--- src/lobbys/domain/models.py
+++ src/lobbys/domain/models.py
@@ -13,13 +13,20 @@ class CreteLobbyRequest(BaseModel):
     max_players: int
     password: Optional[str] = None
 
+class RoomInfo(BaseModel):
+    roomID: int
+    roomName: str
+    minPlayers: int
+    maxPlayers: int
+    players: List[Dict[str, Any]] 
+
 class Status(BaseModel):
     currentTurn: int
     turnOrder: List[int]
     lastColor: int
     board: List[List[int]]
     handMovementCards: List[int]
-    players: List[Dict[str, Any]]  # Lista de diccionarios para los jugadores
+    players: List[Dict[str, Any]]
 
 class WebsocketsUpdateResponse(BaseModel):
     msg: str
