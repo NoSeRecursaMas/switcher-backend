fbb5ac54cd1fd1c058620a7255cdb8fda50e2b79
Lozano-Benjamin <benjalozano13@outlook.com>
2024-09-22 15:02:48 -0300

    SW-8 Creado modelo para respuesta de websocket

--- src/lobbys/domain/models.py
+++ src/lobbys/domain/models.py
@@ -1,3 +1,4 @@
+from typing import Dict, List, Any, Optional
 from pydantic import BaseModel
 
 
@@ -10,4 +11,16 @@ class CreteLobbyRequest(BaseModel):
     lobbyName: str
     min_players: int
     max_players: int
-    password: str = None
+    password: Optional[str] = None
+
+class Status(BaseModel):
+    currentTurn: int
+    turnOrder: List[int]
+    lastColor: int
+    board: List[List[int]]
+    handMovementCards: List[int]
+    players: List[Dict[str, Any]]  # Lista de diccionarios para los jugadores
+
+class WebsocketsUpdateResponse(BaseModel):
+    msg: str
+    status: Status
