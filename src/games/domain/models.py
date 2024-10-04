25004af99a94b55d007c8a86d7ad42d30a29be33
Lozano-Benjamin <benjalozano13@outlook.com>
2024-10-04 15:04:14 -0300

    SW-23 agregado modelo de game

---
+++ src/games/domain/models.py
@@ -0,0 +1,43 @@
+from typing import List, Optional
+from pydantic import BaseModel
+
+from src.players.domain.models import Player
+
+class Board(BaseModel):
+    PosX: int
+    PosY: int
+    Color: str
+
+class LastMovement(BaseModel):
+    PosX1: int
+    PosY1: int
+    PosX2: int
+    PosY2: int
+    Order: int
+    CardID: int
+
+class MovementCard(BaseModel):
+    type: str
+    cardID: int
+    isDiscarded: bool
+
+class FigureCard(BaseModel):
+    type: str
+    cardID: int
+    isBlocked: bool
+
+class Player(BaseModel):
+    playerID: int
+    username: str
+    position: int
+    isActive: bool
+    sizeDeckFigure: int
+    MovementCards: List[MovementCard]
+    FigureCards: List[FigureCard]
+
+class Game(BaseModel):
+    GameID: int
+    Board: List[Board]
+    LastMovement: Optional[LastMovement]
+    ProhibitedColor: Optional[str]
+    players: List[Player]
