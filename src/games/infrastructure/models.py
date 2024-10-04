9aa83c5f6b1d1d99faad6399ba4828ee964fddb4
Lozano-Benjamin <benjalozano13@outlook.com>
2024-10-04 19:06:18 -0300

    SW-23 Actualizacion de tablas BD

--- src/games/infrastructure/models.py
+++ src/games/infrastructure/models.py
@@ -5,41 +5,24 @@ from src.database import Base
 class Game(Base):
     __tablename__ = "games"
 
-    gameID = Column(Integer, primary_key=True, index=True)
     board = Column(JSON)
     lastMovements = Column(JSON, nullable=True)
     prohibitedColor = Column(String, nullable=True)
 
-    players = relationship("PlayerGame", back_populates="games")
+    room = relationship("Room", back_populates="games")
 
     def __repr__(self):
         return f"<Game(gameID={self.gameID}, board={self.board}, lastMovements={self.lastMovements}, prohibitedColor={self.prohibitedColor})>"
-   
-class PlayerGame(Base):
-    __tablename__ = "player_game"
 
-    playerID = Column(Integer, ForeignKey("players.playerID"), primary_key=True)
-    gameID = Column(Integer, ForeignKey("games.gameID"), primary_key=True)
-    position = Column(Integer, nullable=True)
-    isActive = Column(Boolean, nullable=True)
-    sizeDeckFigure = Column(Integer, nullable=True)
-
-    game = relationship("Game", back_populates="players")
-    player = relationship("Player", back_populates="games")
-    figureCards = relationship("FigureCard", back_populates="playerGame")
-    playerhand
-    movementCards = relationship("MovementCard", back_populates="playerGame")
-    
-    def __repr__(self):
-        return f"<PlayerGame(playerID={self.playerID}, gameID={self.gameID})>"
     
 class FigureCard(Base):
     __tablename__ = "figure_cards"
 
     cardID = Column(Integer, primary_key=True)
     type = Column(String, nullable=True)
-    isBlocked = Column(Boolean, nullable=True)
-    playerGameID = Column(Integer, ForeignKey("player_game.playerID"))
+    isBlocked = Column(Boolean, default=False)
+    isPlayable = Column(Boolean, default=False)
+    player = relationship("PlayerDeck", back_populates="players")
 
     def __repr__(self):
         return f"<FigureCard(cardID={self.cardID}, type={self.type}, isBlocked={self.isBlocked})>"
@@ -50,8 +33,9 @@ class MovementCard(Base):
 
     cardID = Column(Integer, primary_key=True)
     type = Column(String, nullable=True)
-    isDiscarded = Column(Boolean, nullable=True)
-    playerGameID = Column(Integer, ForeignKey("player_game.playerID"))
+    isPlayable = Column(Boolean, default=False)
+    isDiscarded = Column(Boolean, default=False)
+    player = relationship("PlayerDeck", back_populates="players")
 
 
     def __repr__(self):
