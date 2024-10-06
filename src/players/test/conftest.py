1b9c964ec8a4a3d069602578be5d82cbc1b2cb2a
Facundo Ayala <ayalafacu00@gmail.com>
2024-10-06 19:42:31 -0300

    SW-53 Creacion de base de datos en memoria

---
+++ src/players/test/conftest.py
@@ -0,0 +1,27 @@
+from src.main import app
+from src.database import get_db
+from sqlalchemy import create_engine, StaticPool
+from sqlalchemy.orm import sessionmaker
+from src.database import Base
+
+from src.main import app
+from fastapi.testclient import TestClient
+
+
+import pytest
+
+
+engine = create_engine("sqlite:///:memory:", connect_args={"check_same_thread": False}, poolclass=StaticPool)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
