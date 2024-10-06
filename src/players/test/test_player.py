baabb1cf185b91ccaf07eab7db6541f1a9e403b7
Facundo Ayala <ayalafacu00@gmail.com>
2024-10-06 19:50:56 -0300

    SW-53 Refactorizacion de test de players

--- src/players/test/test_player.py
+++ src/players/test/test_player.py
@@ -1,17 +1,17 @@
-import pytest
-
-from src.shared.mocks import create_mock_player, mock_db, new_mock
 
 
-def test_create_player(new_mock, mock_db):
-    mock_player = create_mock_player(mock_db, playerID=1, username="mensio")
-    response = new_mock.post("/players", json=mock_player)
+def test_create_player(client, test_db):
+
+    player_data = {"playerID" : 1, "username": "mensio"}
+
+    response = client.post("/players", json=player_data )
+
     assert response.status_code == 201
     assert response.json() == {"playerID": 1, "username": "mensio"}
 
 
-def test_create_player_invalid_size(new_mock):
-    response = new_mock.post("/players", json={"username": ""})
+def test_create_player_invalid_size(client, test_db):
+    response = client.post("/players", json={"username": ""})
     assert response.status_code == 422
     assert (
         response.json().get("detail")[0]["msg"]
@@ -19,9 +19,9 @@ def test_create_player_invalid_size(new_mock):
     )
 
 
-def test_create_player_long_name(new_mock):
+def test_create_player_long_name(client, test_db):
     long_name = "A" * 33
-    response = new_mock.post("/players", json={"username": long_name})
+    response = client.post("/players", json={"username": long_name})
     assert response.status_code == 422
     assert (
         response.json().get("detail")[0]["msg"]
@@ -29,38 +29,50 @@ def test_create_player_long_name(new_mock):
     )
 
 
-def test_create_player_non_ascii(new_mock):
-    response = new_mock.post("/players", json={"username": "nombre_con_ñ"})
+def test_create_player_non_ascii(client, test_db):
+    response = client.post("/players", json={"username": "nombre_con_ñ"})
     assert response.status_code == 422
     assert response.json().get("detail")[0]["msg"] == "El username proporcionado contiene caracteres no permitidos."
 
 
-def test_create_player_one_character(new_mock, mock_db):
-    mock_player = create_mock_player(mock_db, playerID=1, username="A")
-    response = new_mock.post("/players", json=mock_player)
+def test_create_player_one_character(client, test_db):
+
+    player_data = {"playerID" : 1, "username": "A"}
+
+    response = client.post("/players", json=player_data)
+
     assert response.status_code == 201
     assert response.json() == {"playerID": 1, "username": "A"}
 
 
-def test_create_player_with_spaces(new_mock, mock_db):
-    mock_player = create_mock_player(mock_db, playerID=1, username="S A N   T I")
-    response = new_mock.post("/players", json=mock_player)
+def test_create_player_with_spaces(client, test_db):
+
+    player_data = {"playerID" : 1, "username": "S A N   T I"}
+
+    response = client.post("/players", json=player_data)
+
     assert response.status_code == 201
     assert response.json() == {"playerID": 1, "username": "S A N   T I"}
 
 
-def test_create_two_players_with_same_name(new_mock, mock_db):
-    mock_player_1 = create_mock_player(mock_db, playerID=1, username="mensio")
-    response1 = new_mock.post("/players", json=mock_player_1)
+def test_create_two_players_with_same_name(client, test_db):
+
+    player_data = {"playerID" : 1, "username": "mensio"}
+         
+    response1 = client.post("/players", json=player_data)      
+
     assert response1.status_code == 201
     player1 = response1.json()
     assert player1["username"] == "mensio"
 
-    mock_player_2 = create_mock_player(mock_db, playerID=2, username="mensio")
-    response2 = new_mock.post("/players", json=mock_player_2)
+    player_data2 = {"playerID" : 2, "username": "mensio"}
+
+    response2 = client.post("/players", json=player_data2)
+
     assert response2.status_code == 201
     player2 = response2.json()
 
     assert player1["playerID"] == 1
     assert player2["playerID"] == 2
+    
     assert player1["playerID"] != player2["playerID"]
