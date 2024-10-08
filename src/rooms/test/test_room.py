ad64d39d7985fd58728e306ead3475b440cd844c
Fabrizio-Longhi <fabrizio.longhi@mi.unc.edu.ar>
2024-10-07 22:18:55 -0300

    SW-53 factorizacion test room

--- src/rooms/test/test_room.py
+++ src/rooms/test/test_room.py
@@ -2,7 +2,8 @@
 from src.players.infrastructure.models import Player as PlayerDB
 from src.conftest import TestingSessionLocal
 from src.conftest import override_get_db
-from src.rooms.infrastructure.models import PlayerRoom
+from src.rooms.infrastructure.models import PlayerRoom as PlayerRoomDB
+from src.rooms.infrastructure.models import Room as RoomDB
 # import pytest
 # from fastapi import WebSocket
 
@@ -267,47 +268,39 @@ def test_create_rooms_with_same_name(client, test_db):
     assert response_2.json() == {"roomID": 2}
     assert response_1.json() != response_2.json()
 
-def test_get_two_rooms(client,test_db):
-    db = next(override_get_db())
-    player1 = PlayerDB(username="player1")
-    db.add(player1)
-    db.commit()
-
-
-
-    data_room= {
-        "playerID": player1.playerID,
-        "roomName": "test_room",
-        "minPlayers": 2,
-        "maxPlayers": 4,
-    }
-    response_1 = client.post("/rooms/", json=data_room)
-    assert response_1.status_code == 201
-    assert response_1.json() == {"roomID": 1}
+def test_get_all_rooms(client, test_db):
 
     db = next(override_get_db())
-    player2 = PlayerDB(username="player2")
-    db.add(player2)
+    players = [PlayerDB(username=f"player{i}") for i in range(1, 6)]
+    db.add_all(players)
     db.commit()
 
-    data_room2 = {
-        "playerID": player2.playerID,
-        "roomName": "test_room2",
-        "minPlayers": 2,
-        "maxPlayers": 4,
-    }
-    response_2 = client.post("/rooms/", json=data_room2)
-    assert response_2.status_code == 201
-    assert response_2.json() == {"roomID": 2}
-   
+    room1 = RoomDB(roomName="test_room1", minPlayers=2, maxPlayers=4, hostID = players[0].playerID)
+    room2 = RoomDB(roomName="test_room2", minPlayers=2, maxPlayers=4, hostID = players[1].playerID)
+
+    db.add_all([room1, room2])
+    db.commit()
+
+    players_room_relations = [
+        PlayerRoomDB(playerID=players[0].playerID, roomID=room1.roomID),
+        PlayerRoomDB(playerID=players[1].playerID, roomID=room2.roomID),
+        PlayerRoomDB(playerID=players[2].playerID, roomID=room1.roomID),
+        PlayerRoomDB(playerID=players[3].playerID, roomID=room2.roomID),
+        PlayerRoomDB(playerID=players[4].playerID, roomID=room2.roomID),
+    ]
+
+    db.add_all(players_room_relations)
+    db.commit()
+
     response = client.get("/rooms/")
+
     assert response.status_code == 200
     assert response.json() == [
         {
             "roomID": 1,
-            "roomName": "test_room",
+            "roomName": "test_room1",
             "maxPlayers": 4,
-            "actualPlayers": 1,
+            "actualPlayers": 2,
             "started": False,
             "private": False,
         },
@@ -315,15 +308,77 @@ def test_get_two_rooms(client,test_db):
             "roomID": 2,
             "roomName": "test_room2",
             "maxPlayers": 4,
-            "actualPlayers": 1,
+            "actualPlayers": 3,
             "started": False,
             "private": False,
-        }
+        },
     ]
 
-def test_get_empty_room(client,test_db):
+
+def test_get_four_rooms(client, test_db):
+
+    db = next(override_get_db())
+    players = [PlayerDB(username=f"player{i}") for i in range(1, 10)]
+    db.add_all(players)
+    db.commit()
+
+    room1 = RoomDB(roomName="test_room1", minPlayers=2, maxPlayers=4, hostID = players[0].playerID)
+    room2 = RoomDB(roomName="test_room2", minPlayers=2, maxPlayers=4, hostID = players[1].playerID)
+    room3 = RoomDB(roomName="test_room3", minPlayers=2, maxPlayers=4, hostID = players[2].playerID)
+    room4 = RoomDB(roomName="test_room4", minPlayers=2, maxPlayers=4, hostID = players[3].playerID)
+
+    db.add_all([room1, room2, room3, room4])
+    db.commit()
+
+    players_room_relations = [
+        PlayerRoomDB(playerID=players[0].playerID, roomID=room1.roomID),
+        PlayerRoomDB(playerID=players[1].playerID, roomID=room2.roomID),
+        PlayerRoomDB(playerID=players[2].playerID, roomID=room3.roomID),
+        PlayerRoomDB(playerID=players[3].playerID, roomID=room4.roomID),
+        PlayerRoomDB(playerID=players[4].playerID, roomID=room2.roomID),
+        PlayerRoomDB(playerID=players[5].playerID, roomID=room3.roomID),
+        PlayerRoomDB(playerID=players[6].playerID, roomID=room4.roomID),
+        PlayerRoomDB(playerID=players[7].playerID, roomID=room1.roomID),
+        PlayerRoomDB(playerID=players[8].playerID, roomID=room2.roomID),
+    ]
+
+    db.add_all(players_room_relations)
+    db.commit()
+
     response = client.get("/rooms/")
+
     assert response.status_code == 200
-    assert response.json() == []
-
-
+    assert response.json() == [
+        {
+            "roomID": 1,
+            "roomName": "test_room1",
+            "maxPlayers": 4,
+            "actualPlayers": 2,
+            "started": False,
+            "private": False,
+        },
+        {
+            "roomID": 2,
+            "roomName": "test_room2",
+            "maxPlayers": 4,
+            "actualPlayers": 3,
+            "started": False,
+            "private": False,
+        },
+        {
+            "roomID": 3,
+            "roomName": "test_room3",
+            "maxPlayers": 4,
+            "actualPlayers": 2,
+            "started": False,
+            "private": False,
+        },
+        {
+            "roomID": 4,
+            "roomName": "test_room4",
+            "maxPlayers": 4,
+            "actualPlayers": 2,
+            "started": False,
+            "private": False,
+        },
+    ]
