01d72651ccdfa55f58469d17d8d3caf1759391bd
Lozano-Benjamin <benjalozano13@outlook.com>
2024-10-14 15:41:57 -0300

    SW-23 Testing listo

--- src/games/test/test_game.py
+++ src/games/test/test_game.py
@@ -1,9 +1,11 @@
+import json
 from src.conftest import override_get_db
 from src.games.infrastructure.models import FigureCard as FigureCardDB
 from src.games.infrastructure.models import MovementCard as MovementCardDB
 from src.players.infrastructure.models import Player as PlayerDB
 from src.rooms.infrastructure.models import PlayerRoom as PlayerRoomDB
 from src.rooms.infrastructure.models import Room as RoomDB
+from src.games.infrastructure.models import Game as GameDB
 
 
 def create_game_generalization_two_players(client, test_db):
@@ -173,31 +175,101 @@ def test_create_game_send_update_room_list_ws(client, test_db):
         response.json() == {"gameID": 1}
 
 
+
 # - Testear que no se crean más de 2 cartas de figuras iguales por cada tipo
 def test_create_game_figure_cards_unique(client, test_db):
-    pass
+    db, players, room = create_game_generalization_two_players(client, test_db)
+
+    response = client.post(f"/games/{room.roomID}", json={"playerID": players[0].playerID})
+
+    figure_cards = db.query(FigureCardDB).filter(FigureCardDB.gameID == 1).all()
+    figure_cards_count_by_type = {}
+    for card in figure_cards:
+        if card.type not in figure_cards_count_by_type:
+            figure_cards_count_by_type[card.type] = 0
+        figure_cards_count_by_type[card.type] += 1
+
+    for count in figure_cards_count_by_type.values():
+        assert count == 2
+    
 
 
 # - Testear que no se crean más de 7 cartas de movimiento iguales por cada tipo
 def test_create_game_movement_cards_unique(client, test_db):
-    pass
+    db, players, room = create_game_generalization_two_players(client, test_db)
+
+    response = client.post(f"/games/{room.roomID}", json={"playerID": players[0].playerID})
+
+    movement_cards = db.query(MovementCardDB).filter(MovementCardDB.gameID == 1).all()
+    movement_cards_count_by_type = {}
+    for card in movement_cards:
+        if card.type not in movement_cards_count_by_type:
+            movement_cards_count_by_type[card.type] = 0
+        movement_cards_count_by_type[card.type] += 1
+    
+    for count in movement_cards_count_by_type.values():
+        assert count <= 7
 
 
 # - Testear que se crea un tablero con 9 celdas de cada color
 def test_create_game_board(client, test_db):
-    pass
+    db, players, room = create_game_generalization_two_players(client, test_db)
+
+    response = client.post(f"/games/{room.roomID}", json={"playerID": players[0].playerID})
+
+    game = db.query(GameDB).get(1)
+    board = json.loads(game.board)  
+
+    color_count = {}
+    for cell in board:
+        if cell["color"] not in color_count:
+            color_count[cell["color"]] = 0
+        color_count[cell["color"]] += 1
+    
+    for count in color_count.values():
+        assert count == 9
 
 
 # - Testear que se asigna el turno de los jugadores correctamente
 def test_create_game_turn_order(client, test_db):
-    pass
+    db, players, room = create_game_generalization_two_players(client, test_db)
+
+    response = client.post(f"/games/{room.roomID}", json={"playerID": players[0].playerID})
+
+    game = db.query(GameDB).get(1)
+    players = db.query(PlayerRoomDB).filter(PlayerRoomDB.roomID == room.roomID).all()
+
+    assert game.posEnabledToPlay == 1
+    assert len(players) == 2
+    assert players[0].position == players[0].position
 
 
 # - Testear que se le asigna la cantidad correcta de cartas figura visibles y no visibles a cada jugador
 def test_create_game_player_cards(client, test_db):
-    pass
+    db, players, room = create_game_generalization_two_players(client, test_db)
+    response = client.post(f"/games/{room.roomID}", json={"playerID": players[0].playerID})
 
+    figure_cards = db.query(FigureCardDB).filter(FigureCardDB.gameID == 1).all()
+    movement_cards = db.query(MovementCardDB).filter(MovementCardDB.gameID == 1).all()
 
-# - Testear que se le asigna la cantidad correcta de cartas movimiento a cada jugador
-def test_create_game_player_figures(client, test_db):
-    pass
+    figure_cards_by_player = {}
+    playable_figure_cards_by_player = {}
+    movements_cards_by_player = {}
+
+    for card in figure_cards:
+        if card.playerID not in figure_cards_by_player:
+            figure_cards_by_player[card.playerID] = 0
+            playable_figure_cards_by_player[card.playerID] = 0
+        figure_cards_by_player[card.playerID] += 1
+        if card.isPlayable:
+            playable_figure_cards_by_player[card.playerID] += 1
+
+    for card in movement_cards:
+        if card.playerID not in movements_cards_by_player:
+            movements_cards_by_player[card.playerID] = 0
+        movements_cards_by_player[card.playerID] += 1
+    
+    for playerID in figure_cards_by_player.keys():
+        assert playable_figure_cards_by_player[playerID] == 3
+        assert movements_cards_by_player[playerID] == 3
+
