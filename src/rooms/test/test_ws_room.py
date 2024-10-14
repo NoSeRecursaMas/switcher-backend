32af4875fbcc9b0b323ebf508271e282851629ac
GonzaCanavesio <gonzalo.canavesio@mi.unc.edu.ar>
2024-10-13 22:11:23 -0300

    SW-56 Arreglados los tests que se rompieron con la correccion de bugs

--- src/rooms/test/test_ws_room.py
+++ src/rooms/test/test_ws_room.py
@@ -94,37 +94,40 @@ def test_close_first_connection_if_player_open_second(client, test_db):
     ])
     db.commit()
     with pytest.raises(WebSocketDisconnect) as e:
-        with client.websocket_connect("/rooms/1") as websocket:
+        with client.websocket_connect("/rooms/1/1") as websocket:
             data = websocket.receive_json()
             assert data["type"] == "status"
-            assert data["payload"] == [
-                {
-                    "roomID": 1,
-                    "roomName": "test room",
-                    "maxPlayers": 4,
-                    "actualPlayers": 2,
-                    "started": False,
-                    "private": False,
-                },
-            ]
+            assert data["payload"] == {
+                "roomID": 1,
+                "roomName": "test room",
+                "minPlayers": 2,
+                "maxPlayers": 4,
+                "hostID": 1,
+                "players": [
+                    {"playerID": 1, "username": "test user"},
+                    {"playerID": 2, "username": "test user 2"},
+                ],
+            }
+                
 
-            with client.websocket_connect("/rooms/1") as websocket2:
+            with client.websocket_connect("/rooms/1/1") as websocket2:
                 data = websocket2.receive_json()
                 assert data["type"] == "status"
-                assert data["payload"] == [
-                    {
-                        "roomID": 1,
-                        "roomName": "test room",
-                        "maxPlayers": 4,
-                        "actualPlayers": 2,
-                        "started": False,
-                        "private": False,
-                    },
-                ]
+                assert data["payload"] == {
+                    "roomID": 1,
+                    "roomName": "test room",
+                    "minPlayers": 2,
+                    "maxPlayers": 4,
+                    "hostID": 1,
+                    "players": [
+                        {"playerID": 1, "username": "test user"},
+                        {"playerID": 2, "username": "test user 2"},
+                    ],
+                }
+                    
 
                 websocket.receive_json()
 
-    assert e.value.code == 1000
+    assert e.value.code == 4005
     assert e.value.reason == "Conexión abierta en otra pestaña"
-
 
