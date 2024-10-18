274aea0d727d79d2894a5aa6e8d720f8b1f4f651
Facundo Ayala <ayalafacu00@gmail.com>
2024-10-18 13:41:50 -0300

    SW-47 Modificado servicio de leave_room para cancelar partida si es host

--- src/rooms/application/service.py
+++ src/rooms/application/service.py
@@ -34,13 +34,22 @@ class RoomService:
         await self.player_domain_service.validate_player_exists(playerID)
         await self.room_domain_service.validate_room_exists(roomID)
         await self.room_domain_service.validate_player_in_room(playerID, roomID)
-        self.room_domain_service.validate_player_is_not_owner(playerID, roomID)
         await self.room_domain_service.validate_game_not_started(roomID)
 
-        self.room_repository.remove_player_from_room(playerID=playerID, roomID=roomID)
+        isHost = self.room_repository.is_owner(playerID, roomID)
+
+        if isHost:
+            self.room_repository.remove_player_from_room(playerID=playerID, roomID=roomID)
+            await self.room_repository.broadcast_room_cancellation(roomID)
+            await self.room_repository.disconnect_player(roomID, playerID)
+            self.room_repository.delete_and_clean(roomID)
+        else:
+            self.room_domain_service.validate_player_is_not_owner(playerID, roomID)
+            self.room_repository.remove_player_from_room(playerID=playerID, roomID=roomID)
+            await self.room_repository.disconnect_player(roomID, playerID)
+            await self.room_repository.broadcast_status_room(roomID)
 
         await self.room_repository.broadcast_status_room_list()
-        await self.room_repository.broadcast_status_room(roomID)
 
     async def join_room(self, roomID: int, playerID: int) -> None:
         await self.player_domain_service.validate_player_exists(playerID)
