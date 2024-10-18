ffebb3b08364901c37027a1734bdc130e58d2798
Facundo Ayala <ayalafacu00@gmail.com>
2024-10-18 13:50:32 -0300

    SW-47 Agregados tests

--- src/rooms/test/test_leave_room.py
+++ src/rooms/test/test_leave_room.py
@@ -85,14 +85,7 @@ def test_leave_room_room_not_found(client, test_db):
         ]
     )
     db.commit()
-
-    # player1 = PlayerDB(username="player1")
-    # db.add(player1)
-    # db.commit()
-    # player2 = PlayerDB(username="player2")
-    # db.add(player2)
-    # db.commit()
-
+    
     data_room = {
         "playerID": 1,
         "roomName": "test_room",
@@ -112,30 +105,6 @@ def test_leave_room_room_not_found(client, test_db):
     response_leave = client.put("/rooms/3/leave", json=data_leave_room)
     assert response_leave.status_code == 404
     assert response_leave.json() == {"detail": "La sala no existe."}
-
-
-def test_leave_room_owner(client, test_db):
-    db = next(override_get_db())
-    player1 = PlayerDB(username="player1")
-    db.add(player1)
-    db.commit()
-
-    data_room = {
-        "playerID": player1.playerID,
-        "roomName": "test_room",
-        "minPlayers": 2,
-        "maxPlayers": 4,
-    }
-    response_1 = client.post("/rooms/", json=data_room)
-    assert response_1.status_code == 201
-    assert response_1.json() == {"roomID": 1}
-
-    data_leave_room = {"playerID": player1.playerID}
-
-    response_leave = client.put("/rooms/1/leave", json=data_leave_room)
-    assert response_leave.status_code == 403
-    assert response_leave.json() == {"detail": "El propietario no puede abandonar la sala."}
-
 
 def test_leave_room_send_update_ws_room_list(client, test_db):
     db = next(override_get_db())
@@ -257,3 +226,70 @@ def test_leave_room_game_started(client, test_db):
     response_leave = client.put("/rooms/1/leave", json=player_id)
     assert response_leave.status_code == 403
     assert response_leave.json() == {"detail": "La partida ya ha comenzado."}
+
+def test_leave_room_host(client, test_db):
+    db = next(override_get_db())
+    players = [PlayerDB(username=f"player{i}") for i in range(1, 3)]
+    db.add_all(players)
+    db.commit()
+
+    room = RoomDB(roomName="test_room", minPlayers=2, maxPlayers=4, hostID=players[0].playerID)
+    db.add(room)
+    db.commit()
+
+    players_room_relations = [
+        PlayerRoomDB(playerID=players[0].playerID, roomID=room.roomID),
+        PlayerRoomDB(playerID=players[1].playerID, roomID=room.roomID),
+    ]
+    db.add_all(players_room_relations)
+    db.commit()
+
+    player_id = {"playerID": players[0].playerID}
+
+    response_leave = client.put("/rooms/1/leave", json=player_id)
+
+    players = db.query(PlayerDB).join(PlayerRoomDB).filter(PlayerRoomDB.roomID == 1).all()
+    players_list = [{"playerID": str(player.playerID), "username": player.username} for player in players]
+
+    assert response_leave.status_code == 200
+    assert player_id["playerID"] not in [player["playerID"] for player in players_list]
+
+    assert db.query(RoomDB).filter(RoomDB.roomID == 1).first() is None
+    assert db.query(PlayerRoomDB).filter(PlayerRoomDB.roomID == 1).first() is None
+
+
+def test_leave_room_not_host(client, test_db):
+    db = next(override_get_db())
+    players = [PlayerDB(username=f"player{i}") for i in range(1, 3)]
+    db.add_all(players)
+    db.commit()
+
+    room = RoomDB(roomName="test_room", minPlayers=2, maxPlayers=4, hostID=players[0].playerID)
+    db.add(room)
+    db.commit()
+
+    players_room_relations = [
+        PlayerRoomDB(playerID=players[0].playerID, roomID=room.roomID),
+        PlayerRoomDB(playerID=players[1].playerID, roomID=room.roomID),
+    ]
+    db.add_all(players_room_relations)
+    db.commit()
+
+    player_id = {"playerID": players[1].playerID}
+
+    response_leave = client.put("/rooms/1/leave", json=player_id)
+
+    players = db.query(PlayerDB).join(PlayerRoomDB).filter(PlayerRoomDB.roomID == 1).all()
+    players_list = [{"playerID": str(player.playerID), "username": player.username} for player in players]
+
+    assert response_leave.status_code == 200
+    assert player_id["playerID"] not in [player["playerID"] for player in players_list]
+
+    assert db.query(RoomDB).filter(RoomDB.roomID == 1).first() is not None
+    assert (
+        db.query(PlayerRoomDB)
+        .filter(PlayerRoomDB.roomID == 1)
+        .filter(PlayerRoomDB.playerID == player_id["playerID"])
+        .first()
+        is None
+    )
