29884c617ed33a7fc0cba47445cc76da451471b1
Facundo Ayala <ayalafacu00@gmail.com>
2024-11-11 22:39:55 -0300

    bugfix_SW-82 Agregados tests ws para subir coverage

--- src/games/test/test_ws_game.py
+++ src/games/test/test_ws_game.py
@@ -1,14 +1,14 @@
 import json
 
 import pytest
-from fastapi.websockets import WebSocketDisconnect
+from fastapi.websockets import WebSocketDisconnect, WebSocket
 
 from src.conftest import override_get_db
 from src.games.infrastructure.models import Game as GameDB
 from src.players.infrastructure.models import Player as PlayerDB
 from src.rooms.infrastructure.models import PlayerRoom as PlayerRoom
 from src.rooms.infrastructure.models import Room as RoomDB
-
+from src.games.infrastructure.websocket import ConnectionManagerGame
 
 def test_connect_to_game_websocket_user_not_exist(client, test_db):
     db = next(override_get_db())
@@ -192,3 +192,63 @@ def test_close_connection_if_player_open_second(client, test_db):
                 websocket.receive_json()
     assert e.value.code == 4005
     assert e.value.reason == "Conexión abierta en otra pestaña"
+
+def test_send_personal_message(client, test_db):
+    db = next(override_get_db())
+    db.add_all([
+        PlayerDB(playerID=1, username="test user"),
+        RoomDB(roomID=1, roomName="test room", minPlayers=2, maxPlayers=4, hostID=1),
+        PlayerRoom(playerID=1, roomID=1),
+        GameDB(roomID=1, board=json.dumps([{"posX": 0, "posY": 0, "color": "R"} for _ in range(36)])),
+    ])
+    db.commit()
+    with client.websocket_connect("/games/1/1") as websocket:
+        websocket.receive_json() 
+        message = {"type": "msg", "payload": "Hello Player"}
+        websocket.send_json(message)
+        data = websocket.receive_json()
+        assert data == message
+
+def test_clean_up_connections(client, test_db):
+    db = next(override_get_db())
+    db.add_all([
+        PlayerDB(playerID=1, username="test user"),
+        RoomDB(roomID=1, roomName="test room", minPlayers=2, maxPlayers=4, hostID=1),
+        PlayerRoom(playerID=1, roomID=1),
+        GameDB(roomID=1, board=json.dumps([{"posX": 0, "posY": 0, "color": "R"} for _ in range(36)])),
+    ])
+    db.commit()
+    with client.websocket_connect("/games/1/1") as websocket:
+        websocket.receive_json()  
+        manager = ConnectionManagerGame()
+        manager.clean_up()
+        assert not manager.active_connections
+
+def test_broadcast_message(client, test_db):
+    db = next(override_get_db())
+    db.add_all([
+        PlayerDB(playerID=1, username="test user 1"),
+        PlayerDB(playerID=2, username="test user 2"),
+        RoomDB(roomID=1, roomName="test room", minPlayers=2, maxPlayers=4, hostID=1),
+        PlayerRoom(playerID=1, roomID=1),
+        PlayerRoom(playerID=2, roomID=1),
+        GameDB(roomID=1, board=json.dumps([{"posX": 0, "posY": 0, "color": "R"} for _ in range(36)])),
+    ])
+    db.commit()
+
+
+    with client.websocket_connect("/games/1/1") as websocket1, \
+         client.websocket_connect("/games/2/1") as websocket2:
+        
+        websocket1.receive_json()
+        websocket2.receive_json()
+    
+        broadcast_message = {"type": "msg", "payload": "Game broadcast message"}
+
+        websocket1.send_json(broadcast_message)
+        websocket2.send_json(broadcast_message)
+
+        data1 = websocket1.receive_json()
+        data2 = websocket2.receive_json()
+        assert data1 == broadcast_message
+        assert data2 == broadcast_message
